5e5f913ad25f0e6e41b7bba220a31d5acc09be66
김덕근 <deokk91@gmail.com>
2024-05-25 17:04:57 +0900

    H2_DB_Connection

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,40 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     // 자바 코드로 직접 스프링 빈 등록하기
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    /**
+     * 개방-폐쇄 원칙(OCP, Open-Close Principle)
+     * 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+     * SpringConfig를 통해 Bean 설정 변경
+     * MemoryMemberRepository 연결을 끊고, JdbcMemberRepository를 연결
+     */
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
